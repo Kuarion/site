337813ee78d80a39bbdc3a1723084cab436f11c6
Gustanol <gualeixos456@gmail.com>
2025-04-11 23:17:27 -0300

    feat: adding User entity and Roles enum

---
+++ src/main/java/com/kuarion/backend/roles/Roles.java
@@ -0,0 +1,31 @@
+import com.kuarion.backend.roles;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Roles implements GrantedAuthority {
+  ADMIN("ROLE_ADMIN"),
+  USER("ROLE_USER"),
+  ENTERPRISE("ROLE_ENTERPRISE");
+
+  private final String role;
+
+  UserRole(String role) {
+    this.role = role;
+  }
+
+  // require method from GrantedAuthority. It returns the string "role"
+  @Override
+  public String getAuthority() {
+    return this.role;
+  }
+  
+  // method to get a role (constant enum) from a string
+  public static Roles fromString(String role) {
+    for (Roles roleValue : Roles.values()) {
+        if (roleValue.role.equalsIgnoreCase(role)) {
+            return roleValue;
+        }
+    }
+    throw new IllegalArgumentException("Invalid role: " + role);
+  }
+}
