1c292831c790e0a959c5a68014cda646b0832ec8
Gustanol <gualeixos456@gmail.com>
2025-04-12 15:06:14 -0300

    feat: creating AuthenticationService that implements UserDetailsService. It will be used by an AuthenticationProvider

---
+++ src/main/java/com/kuarion/backend/service/AuthenticationService.java
@@ -0,0 +1,44 @@
+package com.kuarion.backend.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.kuarion.backend.entities.User;
+import com.kuarion.backend.repositories.UserRepository;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+  private UserRepository userRepository;
+  
+  // Dependency Injection (UserRepository)
+  public AuthenticationService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+  
+  // required UserDetailsService method. It loads an user for AuthenticationProvider
+  @Override
+  public UserDetails loadUserByUsername(String user) {
+    // the method uses username or email
+    
+    // by default, it loads an user by username
+    Optional<User> userDetails = this.userRepository.findByUsername(user);
+    
+    // if 'user' is not username, it tries to find by email
+    if (userDetails.isEmpty()) {
+        userDetails = this.userRepository.findByEmail(user);
+    }
+    
+    // if 'user' is not email, it throws an exception
+    if (userDetails.isEmpty()) {
+        throw new UsernameNotFoundException("User not found!");
+    }
+    
+    // return the user
+    return userDetails.get();
+  }
+}
