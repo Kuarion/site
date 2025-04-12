cccc53c5b1e018786a8bd51ebc219ec5231f108c
Gustanol <gualeixos456@gmail.com>
2025-04-11 21:19:28 -0300

    feat: Adding initial files

---
+++ src/main/java/com/kuarion/backend/KuarionApplication.java
@@ -0,0 +1,13 @@
+package com.kuarion.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuarionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuarionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuarion/backend/KuarionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuarion.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuarionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
