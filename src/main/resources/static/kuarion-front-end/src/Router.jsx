3a272610997c4789f3410722df8a79175e9e5d33
SD-W1972 <pernalongaesnobe@gmail.com>
2025-04-24 02:56:57 -0300

    fix: fixing React auth router

--- src/main/resources/static/kuarion-front-end/src/Router.jsx
+++ src/main/resources/static/kuarion-front-end/src/Router.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import { Index } from './pages/Index/Index';
 import { Login } from './pages/Login/Login';
 import { NotFound } from './pages/NotFound/NotFound';
+import { Auth } from './pages/Auth';
 
 export function Router() {
     return (
         <Routes>
             <Route path='/' element={<Index />} />
             <Route path='/login' element={<Login />} />
+            <Route path='/auth' element={<Auth />} />
             <Route path='*' element={<NotFound />} />
         </Routes>
     );
